05b2300234f74cb1f20034c45712c46348632818
issamtabbara <44811656+issamtabbara@users.noreply.github.com>
2023-03-19 18:56:18 +0200

    asda

--- src/components/home/home.component.jsx
+++ src/components/home/home.component.jsx
@@ -9,8 +9,9 @@ import Sixt from "../../Images/Home/Sixt.png";
 import EnterpRent from "../../Images/Home/enterprise-rent-a-car-logo 1.png";
 import hertz from "../../Images/Home/Hertz_Car_Rental_logo 3.png";
 import ReferAndEarn from "../../Images/Home/ReferAndEarn.png";
+import CompanyLogo from "../../Images/Home/CompanyLOGO.png";
 import "./home.styles.scss";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
 
 const Home = () => {
   return (
@@ -26,96 +27,102 @@ const Home = () => {
           alt='App store'
         />
       </div>
-      <h1>Your trip starts here!</h1>
-      <div className='Home-WorldMap'>
-        <img
-          src={WorldMap}
-          alt='World Map'
-        />
-      </div>
-      <h1>We’re always here for you</h1>
-      <div className='Home-DownloadFrame'>
-        <img
-          src={DownloadFrame}
-          alt='Mobile app'
-        />
-        <div className='DownloadNow'>
-          <p>You can download the app here</p>
+      <div className='Home-CardContainer'>
+        <h1>Your trip starts here!</h1>
+        <div className='Home-WorldMap'>
           <img
-            src={GooglePlayIcon}
-            alt='Google Play'
-          />
-          <img
-            src={AppStoreIcon}
-            alt='App store'
+            src={WorldMap}
+            alt='World Map'
           />
         </div>
       </div>
-      <h1>We’re the hub that connects all</h1>
-      <div className='Home-CarRentals-Container'>
-        <div>
+      <div className='Home-CardContainer'>
+        <h1>We’re always here for you</h1>
+        <div className='Home-DownloadFrame'>
           <img
-            src={Umbrella}
-            alt='Umbrella'
+            src={DownloadFrame}
+            alt='Mobile app'
           />
-        </div>
-        <div className='Home-CarRentalsLogo'>
-          <Container>
-            <Row md={10}>
-              <Col>
-                <div className='Grid-CarRentLogo'>
-                  <img
-                    src={Avis}
-                    alt='Avis'
-                  />
-                </div>
-              </Col>
-              <Col>
-                <div className='Grid-CarRentLogo'>
-                  <img
-                    src={Sixt}
-                    alt='Sixt'
-                  />
-                </div>
-              </Col>
-            </Row>
-            <Row md={10}>
-              <Col>
-                <div className='Grid-CarRentLogo'>
-                  <img
-                    src={hertz}
-                    alt='Hertz'
-                  />
-                </div>
-              </Col>
-              <Col>
-                <div className='Grid-CarRentLogo'>
-                  <img
-                    src={EnterpRent}
-                    alt='EnterpRent'
-                  />
-                </div>
-              </Col>
-            </Row>
-            <Row md={10}>
-              <Col>
-                <div className='Grid-CarRentLogo'>
-                  <img
-                    src={Alamo}
-                    alt='Alamo'
-                  />
-                </div>
-              </Col>
-              <Col>
-                <div className='Grid-CarRentLogo'>
-                  <p>And more...</p>
-                </div>
-              </Col>
-            </Row>
-          </Container>
+          <div className='DownloadNow'>
+            <p>You can download the app here</p>
+            <img
+              src={GooglePlayIcon}
+              alt='Google Play'
+            />
+            <img
+              src={AppStoreIcon}
+              alt='App store'
+            />
+          </div>
         </div>
       </div>
-      <div>
+      <div className='Home-CardContainer'>
+        <h1>We’re the hub that connects all</h1>
+        <div className='Home-CarRentals-Container'>
+          <div>
+            <img
+              src={Umbrella}
+              alt='Umbrella'
+            />
+          </div>
+          <div className='Home-CarRentalsLogo'>
+            <Container>
+              <Row md={10}>
+                <Col>
+                  <div className='Grid-CarRentLogo'>
+                    <img
+                      src={Avis}
+                      alt='Avis'
+                    />
+                  </div>
+                </Col>
+                <Col>
+                  <div className='Grid-CarRentLogo'>
+                    <img
+                      src={Sixt}
+                      alt='Sixt'
+                    />
+                  </div>
+                </Col>
+              </Row>
+              <Row md={10}>
+                <Col>
+                  <div className='Grid-CarRentLogo'>
+                    <img
+                      src={hertz}
+                      alt='Hertz'
+                    />
+                  </div>
+                </Col>
+                <Col>
+                  <div className='Grid-CarRentLogo'>
+                    <img
+                      src={EnterpRent}
+                      alt='EnterpRent'
+                    />
+                  </div>
+                </Col>
+              </Row>
+              <Row md={10}>
+                <Col>
+                  <div className='Grid-CarRentLogo'>
+                    <img
+                      src={Alamo}
+                      alt='Alamo'
+                    />
+                  </div>
+                </Col>
+                <Col>
+                  <div className='Grid-CarRentLogo'>
+                    <p>And more...</p>
+                  </div>
+                </Col>
+              </Row>
+            </Container>
+          </div>
+        </div>
+      </div>
+      <div className='Home-CardContainer'>
         <h1>Passivity and longevity</h1>
         <div className='Home-ReferAndEarn'>
           <div className='Home-ReferAndEarn-Message'>
@@ -131,6 +138,20 @@ const Home = () => {
               alt='Refer And Earn'
             />
           </div>
+        </div>
+        <div className='Home-Refer-KnowMore'>
+          <h1>
+            Know more about our referral program{" "}
+            <Button variant='primary'> here</Button>!
+          </h1>
+        </div>
+      </div>
+      <div className='Home-CardContainer'>
+        <div className='Home-CompanyLogo'>
+          <img
+            src={CompanyLogo}
+            alt='Company Logo'
+          ></img>
         </div>
       </div>
     </div>
