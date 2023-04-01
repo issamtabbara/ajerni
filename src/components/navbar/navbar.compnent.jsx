dd3d2162f26c0f4a0b075f2ed5b5fe29a24af070
issamtabbara <44811656+issamtabbara@users.noreply.github.com>
2023-04-01 17:03:34 +0300

    sfg

--- src/components/navbar/navbar.compnent.jsx
+++ src/components/navbar/navbar.compnent.jsx
@@ -1,28 +1,36 @@
 import { Fragment } from "react";
 import { ReactComponent as LOGO } from "../../Images/Logo/LOGO.svg";
-import logoutBtn from "../../Images/navbar/LogIn-Btn.svg"
+import logoutBtn from "../../Images/navbar/LogIn-Btn.svg";
 import Navbarbtn from "../Navbar-Btn/NavBar-Btn.component";
-import "./navbar.styles.scss"
+import "./navbar.styles.scss";
 const Navbar = () => {
-    return (
-       <Fragment>
-            <div className="Navbar-Container">
-                <div className="Navbar-Logo">
-                    <LOGO width="27px" height="38px" />
-                    <p>dahherni</p> 
-                </div>
-                <div className="Navbar-Buttons-Container"> 
-                    <Navbarbtn>Program</Navbarbtn>
-                    <Navbarbtn>Contact</Navbarbtn>
-                    <Navbarbtn>Partnership</Navbarbtn>
-                    <Navbarbtn>About us</Navbarbtn>
-                    <Navbarbtn>Blog</Navbarbtn>
-                    <Navbarbtn><img src={logoutBtn} alt="Logout"/></Navbarbtn>
-                </div>
-            </div>
-            <div className="Navbar-LowerBorder"/>
-       </Fragment>
-    );
-}
+  return (
+    <Fragment>
+      <div className='Navbar-Container'>
+        <div className='Navbar-Logo'>
+          <LOGO
+            width='27px'
+            height='38px'
+          />
+          <p>dahherni</p>
+        </div>
+        <div className='Navbar-Buttons-Container'>
+          <Navbarbtn>Program</Navbarbtn>
+          <Navbarbtn>Contact</Navbarbtn>
+          <Navbarbtn>Partnership</Navbarbtn>
+          <Navbarbtn>About us</Navbarbtn>
+          <Navbarbtn>Blog</Navbarbtn>
+          <Navbarbtn>
+            <img
+              src={logoutBtn}
+              alt='Logout'
+            />
+          </Navbarbtn>
+        </div>
+      </div>
+      <div className='Navbar-LowerBorder' />
+    </Fragment>
+  );
+};
 
 export default Navbar;
